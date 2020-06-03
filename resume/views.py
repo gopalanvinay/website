78b8ccc70bb3ac25c05cee34f9ae75c5e1252edb
gopalanvinay <gopalanvinay@gmail.com>
2020-06-03 09:33:00 -0700

    Removed MEDIA_ROOT

--- resume/views.py
+++ resume/views.py
@@ -5,7 +5,7 @@ from django.core.mail import send_mail
 from django.http import FileResponse
 from django.core.mail import EmailMessage
 from django.utils import timezone
-from mySite.settings import MY_EMAIL, MEDIA_ROOT
+from mySite.settings import MY_EMAIL
 import mimetypes
 
 
