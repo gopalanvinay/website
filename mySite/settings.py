b65d6facf58773abfb791e67110470fde8687f10
gopalanvinay <gopalanvinay@gmail.com>
2020-06-03 09:51:06 -0700

    Trying new Base_Dir

--- mySite/settings.py
+++ mySite/settings.py
@@ -14,7 +14,7 @@ from decouple import config
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname((os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,8 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATIC_ROOT = 'resume/static'
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Extra places for collectstatic to find static files.
 
